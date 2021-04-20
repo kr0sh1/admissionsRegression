33a04dd27c115482a59e684ec2a42b575bc3420c
lkltd <him@louisking.co.uk>
2021-04-20 22:12:01 +0100

    First version of a fully written regression model script. Currently the error rate is about 9% and no hyperparameter tuning is done, next version needs to be more optimised.

--- script.py
+++ script.py
@@ -7,12 +7,24 @@ from tensorflow	import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras import layers
+from tensorflow.keras.layers import Dense
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import Normalizer
 from sklearn.compose import ColumnTransformer
 from sklearn.metrics import r2_score
+
+def build_model(features):
+    model = Sequential(name="outputModel")
+    input = tf.keras.Input(shape=(features.shape[1],))
+    model.add(input)
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(1))
+    opt = tf.keras.optimizers.SGD(learning_rate=0.1)
+    model.compile(loss='mse', metrics=['mae'], optimizer=opt)
+    return model
+
 
 admissionData = pd.read_csv("admissions_data.csv")
 
@@ -34,3 +46,11 @@ features_train = ct.fit_transform(features_train)
 features_test = ct.transform(features_test)
 
 # create neural network
+admissionsModel = build_model(features_train)
+print(admissionsModel.summary())
+admissionsModel.fit(features_train, labels_train, epochs=80, batch_size=1, verbose=1)
+
+val_mse, val_mae = None, None
+val_mse, val_mae = admissionsModel.evaluate(features_test, labels_test, verbose=0)
+
+print("MAE: ", val_mae)
